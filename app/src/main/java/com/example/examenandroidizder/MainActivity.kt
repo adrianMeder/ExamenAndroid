7a01f8174d26e22202dac6adfb9e61c8660768f5
adrianMeder <adrian.izderuy@gmail.com>
2023-03-22 16:26:01 -0300

    modify view activity

--- app/src/main/java/com/example/examenandroidizder/MainActivity.kt
+++ app/src/main/java/com/example/examenandroidizder/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.examenandroidizder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
 import com.example.examenandroidizder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        iniComponent()
+    }
+
+    private fun iniComponent() {
+
+        val filter = InputFilter { source, _, _, _, _, _ ->
+            // Only allow alphanumeric characters
+            source.filter { it.isLetterOrDigit() }
+        }
+
+        binding.edtUserName.filters = arrayOf(filter)
     }
 }
